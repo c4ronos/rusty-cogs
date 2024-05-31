b1c3519b04577ab3c958b99ef4b8e2d3e5d70ab6
Katana <110231125+katanadotnet@users.noreply.github.com>
2024-05-31 21:03:46 +0530

    Update

--- banner/__init__.py
+++ banner/__init__.py
@@ -1,5 +1,6 @@
+from redbot.core.bot import Red
+
 from .banner import Banner
 
-
-async def setup(bot):
-    await bot.add_cog(Banner(bot))
+async def setup(bot: Red) -> None:
+    await bot.add_cog(Banner())
