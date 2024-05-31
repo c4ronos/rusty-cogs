dc7fb2f27a86d2bc7cf71a20f7ec9414f750b3fa
Katana <110231125+katanadotnet@users.noreply.github.com>
2024-05-31 21:03:17 +0530

    Update

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,5 +1,6 @@
+from redbot.core.bot import Red
+
 from .avatar import Avatar
 
-
-async def setup(bot):
-    await bot.add_cog(Avatar(bot))
+async def setup(bot: Red) -> None:
+    await bot.add_cog(Avatar())
