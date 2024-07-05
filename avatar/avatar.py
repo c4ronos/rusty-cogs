72465f598c284d2963ecf2e315b881ecd6ee3866
Rusty <110231125+rusty-man@users.noreply.github.com>
2024-07-05 18:57:29 +0530

    add option to change embed color

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,9 +1,15 @@
 import discord
 from typing import Optional, Union
-from redbot.core import app_commands, commands
+from redbot.core import app_commands, commands, Config
 
 class Avatar(commands.Cog):
     """Get a user's avatar."""
+
+    def __init__(self):
+        self.config = Config.get_conf(self, identifier=524188088840)
+        default_global = {"embed_color": None}
+        self.config.register_global(**default_global)
+
 
     @commands.hybrid_command(name="avatar", description="Get a user's avatar")
     @app_commands.describe(user="The user you wish to retrieve the avatar of (optional)")
@@ -16,7 +22,8 @@ class Avatar(commands.Cog):
         """
 
         user = user or ctx.author
-        embed = discord.Embed(color=user.color, title="Avatar")
+        embed_color = await self.config.embed_color() or user.color
+        embed = discord.Embed(color=embed_color, title="Avatar")
 
         try:
             # Attempt to fetch the avatar URL
@@ -24,20 +31,38 @@ class Avatar(commands.Cog):
 
             embed.set_author(name=f"{user.name} ~ {user.display_name}", icon_url=user.avatar.url)
             embed.set_image(url=avatar_url)
-	
         except discord.HTTPException:
-
             embed.description = "No avatar found for this user."
             await ctx.send(embed=embed)
             return
-
-        # - Detect animated avatars (might need sometime) -
-        #is_animated = avatar_url.endswith(".gif")
 
         if ctx.channel.permissions_for(ctx.guild.me).embed_links:
             await ctx.send(embed=embed)
         else:
             await ctx.send("I do not have permission to send embeds in this channel.", ephemeral=True)
 
+
+    @commands.command(name="avatar_embed", description="Embed color for avatar (defaults to role color)")
+    @commands.guild_only()
+    @commands.is_owner()
+    async def avatar_embed(self, ctx: commands.Context, color: str) -> None:
+        """Embed color for avatar (defaults to role color)
+
+        Use a hex color code or 'clear' to reset to the default color.
+        """
+
+        if color.lower() == "clear":
+            await self.config.embed_color.set(None)
+            await ctx.send("Embed color has been reset to default.")
+        else:
+            try:
+                # Only accept hex (and `clear`)
+                embed_color = discord.Color(int(color.lstrip("#"), 16))
+                await self.config.embed_color.set(embed_color.value)
+                await ctx.send(f"Embed color has been set to {color}.")
+            except ValueError:
+                await ctx.send("Invalid hex color code. Please provide a valid hex color code or 'clear'.")
+
+
     async def red_delete_data_for_user(self, **kwargs) -> None:
-	    pass
+        pass
--- banner/banner.py
+++ banner/banner.py
@@ -1,12 +1,18 @@
 import discord
 from typing import Optional, Union
-from redbot.core import app_commands, commands
+from redbot.core import app_commands, commands, Config
 
 class Banner(commands.Cog):
     """Get a user's banner."""
 
+    def __init__(self):
+        self.config = Config.get_conf(self, identifier=245189443860)
+        default_global = {"embed_color": None}
+        self.config.register_global(**default_global)
+
+
     @commands.hybrid_command(name="banner", description="Get a user's banner")
-    @app_commands.describe(user="The user you wish to retrieve the banner of")
+    @app_commands.describe(user="The user you wish to retrieve the banner of (optional)")
     @app_commands.guild_only()
     async def banner(self, ctx: commands.Context, *, user: Optional[Union[discord.Member, discord.User]]) -> None:
         """Returns a user's banner as an embed.
@@ -16,7 +22,8 @@ class Banner(commands.Cog):
         """
 
         user = user or ctx.author
-        embed = discord.Embed(color=user.color, title="Banner")
+        embed_color = await self.config.embed_color() or user.color
+        embed = discord.Embed(color=embed_color, title="Banner")
 
         
         try:
@@ -35,13 +42,33 @@ class Banner(commands.Cog):
         except discord.HTTPException:
             embed.description = "No banner found for this user."
 
-        # - Detect animated banners (might need sometime) -
-        #is_animated = banner_url.endswith(".gif")
-
         if ctx.channel.permissions_for(ctx.guild.me).embed_links:
             await ctx.send(embed=embed)
         else:
             await ctx.send("I do not have permission to send embeds in this channel.", ephemeral=True)
+
+
+    @commands.command(name="banner_embed", description="Embed color for banner (defaults to role color)")
+    @commands.guild_only()
+    @commands.is_owner()
+    async def banner_embed(self, ctx: commands.Context, color: str) -> None:
+        """Embed color for banner (defaults to role color)
+
+        Use a hex color code or 'clear' to reset to the default color.
+        """
+
+        if color.lower() == "clear":
+            await self.config.embed_color.set(None)
+            await ctx.send("Embed color has been reset to default.")
+        else:
+            try:
+                # Only accept hex (and `clear`)
+                embed_color = discord.Color(int(color.lstrip("#"), 16))
+                await self.config.embed_color.set(embed_color.value)
+                await ctx.send(f"Embed color has been set to {color}.")
+            except ValueError:
+                await ctx.send("Invalid hex color code. Please provide a valid hex color code or 'clear'.")
+
 
     async def red_delete_data_for_user(self, **kwargs) -> None:
         pass
