4dc34a256612604b3ca691fa81d03a80c02e97fa
Rusty <110231125+rusty-man@users.noreply.github.com>
2024-07-26 14:25:25 +0530

    [gulag] typing indicator + cooldown

--- gulag/gulag.py
+++ gulag/gulag.py
@@ -10,9 +10,11 @@ class Gulag(commands.Cog):
         self.config.register_guild(gulag_role=None, gulag_channel=None)
         self.original_roles = {}
 
+
     @commands.hybrid_command(name="gulag")
     @app_commands.describe(member="The member to send to the gulag")
     @commands.has_permissions(administrator=True)
+    @commands.cooldown(1, 1, commands.BucketType.user)
     @commands.guild_only()
     async def gulag_member(self, ctx: commands.Context, member: discord.Member):
         """Sends a member to the gulag."""
@@ -35,10 +37,17 @@ class Gulag(commands.Cog):
             await ctx.send("Insufficient permissions (bot/author heirarchy)")
             return
 
-        removable_roles = [role for role in member.roles[1:] if role.id != member.guild.premium_subscriber_role.id]
+        booster_role = None
+        if ctx.guild.premium_subscriber_role:
+            booster_role = ctx.guild.premium_subscriber_role.id
+
+        removable_roles = [role for role in member.roles[1:] if role.id != booster_role and role.managed == False]
         self.original_roles[member.id] = removable_roles
-        await member.remove_roles(*removable_roles)
-        await member.add_roles(gulag_role)
+
+        async with ctx.typing():
+            await member.remove_roles(*removable_roles)
+            await member.add_roles(gulag_role)
+
         await ctx.send(f"{member.mention} has been sent to the gulag.")
         await gulag_channel.send(f"{member.mention} [-](https://tenor.com/view/gulag-survive-welcome-to-the-gulag-gif-27052296)")
 
@@ -46,6 +55,7 @@ class Gulag(commands.Cog):
     @commands.hybrid_command(name="bail")
     @app_commands.describe(member="The member to be released from the gulag")
     @commands.has_permissions(administrator=True)
+    @commands.cooldown(1, 1, commands.BucketType.user)
     @commands.guild_only()
     async def bail_member(self, ctx: commands.Context, member: discord.Member):
         """Releases a member from the gulag."""
@@ -58,14 +68,16 @@ class Gulag(commands.Cog):
             return
 
         if gulag_role in member.roles:
-            await member.remove_roles(gulag_role)
-            await ctx.send(f"{member.mention} has been released from the gulag.")
+            async with ctx.typing():
+                await member.remove_roles(gulag_role)
 
-            if member.id in self.original_roles:
-                await member.add_roles(*self.original_roles[member.id], atomic=True)
-                del self.original_roles[member.id]
-            else:           # Member not in bot's list
-                await ctx.send(f"User's original roles were not found and cannot be added back. You will have to add them manually.")
+                if member.id in self.original_roles:
+                    await member.add_roles(*self.original_roles[member.id], atomic=True)
+                    del self.original_roles[member.id]
+                else:           # Member not in bot's list
+                    await ctx.send(f"User's original roles were not found and cannot be added back. You will have to add them manually.")
+
+            await ctx.send(f"{member.mention} has been released from the gulag.")
         else:
             await ctx.send(f"{member.display_name} is not currently in the gulag.")
 
@@ -80,22 +92,20 @@ class Gulag(commands.Cog):
     @gulagset.command(name="channel")
     async def set_gulag_channel(self, ctx: commands.Context, channel: discord.TextChannel):
         """Configures the gulag channel."""
-        guild = ctx.guild
         if not channel:
             await ctx.send("Channel not found.")
             return
 
-        await self.config.guild(guild).gulag_channel.set(channel.id)
+        await self.config.guild(ctx.guild).gulag_channel.set(channel.id)
         await ctx.send(f"Gulag channel configured as {channel.mention}.")
 
 
     @gulagset.command(name="role")
     async def set_gulag_role(self, ctx: commands.Context, role: discord.Role):
         """Configures the gulag role."""
-        guild = ctx.guild
-        gulag_channel_id = await self.config.guild(guild).gulag_channel()
+        gulag_channel_id = await self.config.guild(ctx.guild).gulag_channel()
 
-        gulag_channel = guild.get_channel(gulag_channel_id)
+        gulag_channel = ctx.guild.get_channel(gulag_channel_id)
         if not gulag_channel:
             await ctx.send("Gulag channel is not configured for this guild.")
             return
@@ -103,27 +113,26 @@ class Gulag(commands.Cog):
             await ctx.send("Role not found.")
             return
 
-        
         # Set permissions across server after channel and role are set.
-        try:
-            await self.update_channel_permissions(guild, role, gulag_channel)
-        except Exception:
-            await ctx.send("Error. Please try again.")
+        async with ctx.typing():
+            try:
+                await self.update_channel_permissions(ctx.guild, role, gulag_channel)
+            except Exception:
+                await ctx.send("Error. Please try again.")
 
-        await self.config.guild(guild).gulag_role.set(role.id)
+        await self.config.guild(ctx.guild).gulag_role.set(role.id)
         await ctx.send(f"Gulag role configured as {role.name}.")
 
 
     @gulagset.command(name="clear")
     async def clear_gulag(self, ctx: commands.Context):
         """Resets the gulag role and channel."""
-        guild = ctx.guild
-        guild_config = self.config.guild(guild)
+        guild_config = self.config.guild(ctx.guild)
         gulag_role_id = await guild_config.gulag_role()
         gulag_channel_id = await guild_config.gulag_channel()
 
-        gulag_role = guild.get_role(gulag_role_id)
-        gulag_channel = guild.get_channel(gulag_channel_id)
+        gulag_role = ctx.guild.get_role(gulag_role_id)
+        gulag_channel = ctx.guild.get_channel(gulag_channel_id)
 
         await guild_config.gulag_role.set(None)
         await guild_config.gulag_channel.set(None)
